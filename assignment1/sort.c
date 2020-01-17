7b030781a266c663d2646f425e8f2174d0780943
Joseph Csoti <josephcsoti@gmail.com>
2020-01-17 14:43:15 -0800

    Imported assignment1

---
+++ assignment1/sort.c
@@ -0,0 +1,23 @@
+#include "merge.h"
+
+/* LEFT index and RIGHT index of the sub-array of ARR[] to be sorted */
+void singleProcessMergeSort(int arr[], int left, int right) 
+{
+  if (left < right) {
+    int middle = (left+right)/2;
+    singleProcessMergeSort(arr, left, middle); 
+    singleProcessMergeSort(arr, middle+1, right); 
+    merge(arr, left, middle, right); 
+  } 
+}
+
+/* 
+ * This function stub needs to be completed
+ */
+void multiProcessMergeSort(int arr[], int left, int right) 
+{
+  // Delete this line, it's only here to fail the code quality check
+  int i = 0;
+
+  // Your code goes here 
+}
