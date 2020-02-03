26640e7932e150b88a34012211dd2506e6877252
Joseph Csoti <josephcsoti@gmail.com>
2020-02-02 19:48:11 -0800

    Working assignment2 (<2.5x speedup)

--- assignment2/sort.c
+++ assignment2/sort.c
@@ -1,23 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include "merge.h"
 
+#define MIN_THREADS 4
+#define MAX_THREADS 32
+
+struct Data {
+  int t_id;
+  int segment_size;
+  int *arr_p;
+};
+
+void* mt_mergesort(void* data);
+void merge_h(int arr[], int left, int right);
+
 /* LEFT index and RIGHT index of the sub-array of ARR[] to be sorted */
-void singleThreadedMergeSort(int arr[], int left, int right) 
-{
+void singleThreadedMergeSort(int arr[], int left, int right) {
   if (left < right) {
-    int middle = (left+right)/2;
-    singleThreadedMergeSort(arr, left, middle); 
-    singleThreadedMergeSort(arr, middle+1, right); 
-    merge(arr, left, middle, right); 
-  } 
+    int middle = (left + right) / 2;
+    singleThreadedMergeSort(arr, left, middle);
+    singleThreadedMergeSort(arr, middle + 1, right);
+    merge(arr, left, middle, right);
+  }
 }
 
-/* 
- * This function stub needs to be completed
+/*
+ * MergeSort implemented using multiple threads
  */
-void multiThreadedMergeSort(int arr[], int left, int right) 
-{
-  // Delete this line, it's only here to fail the code quality check
-  int i = 0;
+void multiThreadedMergeSort(int arr[], int left, int right) {
+  // Find thread number from array size
+  int len = right + 1;
+  int threads_to_use = 4;
+  int segment_size = len / threads_to_use;
 
-  // Your code goes here 
+  // printf("Arr was %d, so threads=%d and seg_size:%d\n", len, threads_to_use, segment_size);
+
+  // Allocate space to hold threads
+  pthread_t threads[threads_to_use];
+  struct Data data[threads_to_use];
+
+  // Create apporriate number of threads
+  for (int t_id = 0; t_id < threads_to_use; t_id++) {
+    // printf("CREATED: T_%d\n", t_id);
+    struct Data d = {t_id, segment_size, &arr[0]};
+    data[t_id] = d;
+    pthread_create(&threads[t_id], NULL, mt_mergesort, (void *) &data[t_id]);
+  }
+
+  // Join all the created threads, and wait for them to finish
+  for (int t_id = 0; t_id < threads_to_use; t_id++) {
+    // printf("JOINED: T_%d\n", t_id);
+    pthread_join(threads[t_id], NULL);
+  }
+
+  // Merge the halves
+
+  int mid = (left+right) / 2;
+  merge_h(arr, left, mid);   // left-left & left-right
+  merge_h(arr, mid+1, right);   // right-left & right-right
+  merge_h(arr, left, right); // left and right
+
+  return;
+}
+
+/*
+ * Thread func to complete mergesort
+ */
+void* mt_mergesort(void* data) {
+  struct Data* d = (struct Data *) data;
+
+  int t_id = d->t_id;
+  int segment_size = d->segment_size;
+  int* arr = d->arr_p;
+
+  // printf("got t_id:%d ss:%d\n", t_id, segment_size);
+
+  int segment_start = segment_size * t_id;
+  int segment_end = (segment_size * (t_id + 1)) - 1;
+
+  // printf("mt_mergesort: start-%d end-%d\n", segment_start, segment_end);
+
+  singleThreadedMergeSort(arr, segment_start, segment_end);
+
+  return 0;
+}
+
+/*
+ * Merge helper, so we dont have to pass in a midpoint everytime
+ */
+void merge_h(int arr[], int left, int right){
+  // printf("Merging %d<=%d<=%d\n", left, (left+right)/2, right);
+  merge(arr, left, (left+right)/2, right);
 }
