7e82f7edadf8be52d22708e9f77f7524f8ce52dc
Joseph Csoti <josephcsoti@gmail.com>
2020-02-19 18:13:24 -0800

    Imported assignment4

---
+++ assignment4/cartman.c
@@ -0,0 +1,26 @@
+
+#include "cartman.h"
+
+/*
+ * You need to implement this function, see cartman.h for details 
+ */
+void arrive(unsigned int cart, enum track track, enum junction junction) 
+{
+}
+
+/*
+ * You need to implement this function, see cartman.h for details 
+ */
+void depart(unsigned int cart, enum track track, enum junction junction) 
+{
+}
+
+/*
+ * You need to implement this function, see cartman.h for details 
+ */
+void cartman() 
+{
+  // Remove, only here to fail code warning test
+  int foo = 1024;
+}
+
---
+++ assignment4/manpage.c
@@ -0,0 +1,22 @@
+
+/*********************************************************************
+ *
+ * Copyright (C) 2020 David C. Harrison. All right reserved.
+ *
+ * You may not use, distribute, publish, or modify this code without 
+ * the express written permission of the copyright holder.
+ *
+ ***********************************************************************/
+
+#include "manpage.h"
+
+/*
+ * See manpage.h for details.
+ *
+ * As supplied, shows random single messages.
+ */
+void manpage() 
+{
+  int mid = getParagraphId();
+  showParagraph();
+}
